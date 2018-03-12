60c8cbd35f38a366ee2d52cb667d21f5a7a81083
PSY <dkelrk1@gmail.com>
2018-03-12 19:39:02 +0900

    Revert "180312 박수영"
    This reverts commit 344cc415c553db4a8f4713953af8ae86fc013ba9.

--- src/controller/psy/PsyController.java
+++ src/controller/psy/PsyController.java
@@ -1,4 +1,4 @@
-package controller.psy;
+package controller;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,22 +17,13 @@ public class PsyController extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//System.out.println("doget으로 들어오는데 성공함");
-		PsyDAO dao = new PsyDAO(req.getServletContext());
-		List<PsyDTO> list=dao.selectList();
-		dao.close();
+		System.out.println("doget으로 들어오는데 성공함");
 		
-		req.setAttribute("list", list);
-		
-		//뷰선택]
-		RequestDispatcher dispatcher=req.getRequestDispatcher("/member/host/HostManage.jsp");
-		//포워딩]
-		dispatcher.forward(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//System.out.println("dopost로 들어오는데 성공함");
+		System.out.println("dopost로 들어오는데 성공함");
 		doGet(req, resp);
 		//폼을 쓰지 않기때문에 여기로 들어올 일은 없다. 만약에 이상한 일이 일어나서 포스트방식으로 들어온다면 바로 doGet으로 인도함.
 	}
