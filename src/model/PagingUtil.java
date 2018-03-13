d19dd41f52f6e81e07c1e542dcd664249f3ab58d
choijihun0202 <choijihun0202@gmail.com>
2018-03-13 17:02:43 +0900

    03-13 NormalManage.jsp 출력

--- src/model/PagingUtil.java
+++ src/model/PagingUtil.java
@@ -46,8 +46,73 @@ public class PagingUtil {
 		return pagingStr;
 	}
 	
-	//박수영
 	public static String pagingBootStrapStyle(int totalRecordCount,int pageSize,int blockPage,int nowPage,String page){
+		
+		String pagingStr= "<nav aria-label=\'Page navigation example\'>\r\n" +
+				"<ul class=\"pagination justify-content-center\"> \r\n";
+		
+		//1.전체 페이지 구하기
+		int totalPage= (int)(Math.ceil(((double)totalRecordCount/pageSize)));
+		
+		int intTemp = ((nowPage - 1) / blockPage) * blockPage + 1;
+
+		//처음 및 이전을 위한 로직
+		if(intTemp != 1){
+			pagingStr+="<li class=\"page-item\">\r\n" + 
+					"<a class=\"page-link href='"+page+"nowPage=1' aria-label=\"Previous\">\r\n" + 
+					"<span aria-hidden=\"true\">&laquo;</span>\r\n" + 
+					"<span class='sr-only'>Previous</span>" + 
+					"</a>\r\n" + 
+					"</li>\r\n" + 
+					"<li >\r\n" + 
+					"<a class=\"page-link\" href='"+page+"nowPage="+(intTemp -blockPage)+"' aria-label=\"Next\" >\r\n" + 
+					"<span aria-hidden=\"true\">&lsaquo;</span>\r\n" + 
+					"<span class=\"sr-only\">Next</span>" +
+					"</a>\r\n" + 
+					"</li>";   
+			
+		}
+		
+		//페이지 표시 제어를 위한 변수
+		int blockCount = 1;
+		
+		//페이지를 뿌려주는 로직
+		//블락 페이지 수만큼 혹은 마지막 페이지가 될때까지 페이지를 표시한다1 
+		while(blockCount <= blockPage && intTemp <= totalPage){  // 페이지 오버 를 체크
+			//현재 페이지를 의미함
+			if(intTemp == nowPage){  
+				pagingStr+="<li class=\"page-item\"><a href='#'><span style='Color:red'>"+intTemp+"</span></a></li>";
+			}
+		     else
+		    	 pagingStr+="<li class=\"page-item\"><a href='"+page+"nowPage="+intTemp+"'>"+intTemp+"</a></li>";
+		       
+			intTemp = intTemp + 1;
+			blockCount = blockCount + 1;
+		}
+
+		//다음 및 마지막을 위한 로직
+		if(intTemp <= totalPage){
+			pagingStr+="<li>\r\n" + 
+					"<a class=\\\"page-link\\\" href='"+page+"nowPage="+intTemp+"'>\r\n" + 
+					"<span aria-hidden=\"true\">&rsaquo;</span>\r\n" + 
+					"<span class='sr-only'>Previous</span>" +
+					"</a>\r\n" + 
+					"</li>\r\n" + 
+					"<li>\r\n" + 
+					"<a class=\\\"page-link\\\" href='"+page+"nowPage="+totalPage+"' >\r\n" + 
+					"<span aria-hidden=\"true\">&raquo;</span>\r\n" + 
+					"<span class=\"sr-only\">Next</span>" +
+					"</a>\r\n" + 
+					"</li>";
+							   
+		}
+		pagingStr+="</ul></nav></div>";
+		return pagingStr;
+	}
+	
+	
+	//박수영
+/*	public static String pagingBootStrapStyle(int totalRecordCount,int pageSize,int blockPage,int nowPage,String page){
 		
 		String pagingStr="<nav><ul class=\"pagination\" >";
 		
@@ -86,11 +151,9 @@ public class PagingUtil {
 		       
 			intTemp = intTemp + 1;
 			blockCount = blockCount + 1;
-		
 		}
 
 		//다음 및 마지막을 위한 로직
-			
 		if(intTemp <= totalPage){
 			pagingStr+="<li>\r\n" + 
 					"<a href='"+page+"nowPage="+intTemp+"'>\r\n" + 
@@ -106,7 +169,8 @@ public class PagingUtil {
 		}
 		pagingStr+="</ul></nav>";
 		return pagingStr;
-	}
+	}*/
+	
 	//박수영
 	
 	public static String pagingImage(){
